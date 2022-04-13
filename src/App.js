3951e082ab19178e939860c4a5c39515c931aa9e
ben <Bin_Huang@syscom.com.tw>
2022-04-13 16:56:18 +0800

    TEST

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+import { Autocomplete, TextField, Button, Grid, Alert } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Grid item>
+              <TextField>aw</TextField>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Grid item xs={12}>
+              <Button variant="contained">確認</Button>
+              <Button color="error" variant="contained">
+                取消
+              </Button>
+              <img src="https://www.nextbank.com.tw/images/paradise/img-paradise_depoit.gif"></img>
+            </Grid>
+          </Grid>
+        </Grid>
       </header>
     </div>
   );
